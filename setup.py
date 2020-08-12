6eeaf3e7c3c664e7d51135a6c34bcac3dad753d2
AbhishekPailwan <68326079+AbhishekPailwan@users.noreply.github.com>
2020-08-12 17:52:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Abhi',                   # Type in your name
   author_email = 'pailwan.abhishek43@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/AbhishekPailwan/abhitry',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/AbhishekPailwan/abhitry/archive/v_01.zip',    # I explain this later on
+  download_url = 'https://github.com/AbhishekPailwan/abhitry/archive/v_02.zip',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
