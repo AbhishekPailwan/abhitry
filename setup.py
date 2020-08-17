050e9fe138786e8d4e771515cafe1148a179a222
AbhishekPailwan <68326079+AbhishekPailwan@users.noreply.github.com>
2020-08-17 15:47:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from distutils.core import setup
 setup(
   name = 'prakmy',         # How you named your package folder (MyLib)
   packages = ['prakmy'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
+  long_description_content_type="text/markdown",
   author = 'Abhi',                   # Type in your name
   author_email = 'pailwan.abhishek43@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/AbhishekPailwan/abhitry',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/AbhishekPailwan/abhitry/archive/v_02.zip',    # I explain this later on
+  download_url = 'https://github.com/AbhishekPailwan/abhitry/archive/v_03.zip',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
