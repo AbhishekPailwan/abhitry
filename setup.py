3c03ce0a5e3ccee6d0ceb52aec7dd7373ee216c5
AbhishekPailwan <68326079+AbhishekPailwan@users.noreply.github.com>
2020-08-17 18:17:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
   version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
+  long_ description ="This Library includes operations like sum,multiply,division etc.",
   long_description_content_type="text/markdown",
   author = 'Abhi',                   # Type in your name
   author_email = 'pailwan.abhishek43@gmail.com',      # Type in your E-Mail
